102ef70be1bf1c5aa0edf4bea02b679fafd1ded4
FedericoPangrazi <federico.pangrazi@gmail.com>
2025-03-18 15:25:36 +0100

    Add files via upload

--- footballers.mjs
+++ footballers.mjs
@@ -1,3 +1,5 @@
+import sqlite from 'sqlite3'
+
 function Footballer(name, age, leagues, teams, nationality, position, career, foot) {
     this.name = name
     this.age = age
@@ -7,6 +9,7 @@ function Footballer(name, age, leagues, teams, nationality, position, career, fo
     this.position = position
     this.career = career
     this.foot = foot
+    
 }
 
 const ronaldo = new Footballer("Cristiano Ronaldo", 40, ["Liga Portugal","Premier League","La Liga", "Serie A","Saudi Pro League"], ["Sporting Lisbona", "Manchester United","Real Madrid","Juventus", "Al-Nassr"], "Portugal", "ATT", 1 , "right")
@@ -15,55 +18,248 @@ const maldini = new Footballer("Paolo Maldini", 57, ["Serie A"], ["AC Milan"], "
 const zidane = new Footballer("Zinedine Zidane", 53, ["Ligue 1","La Liga","Serie A"], ["Cannes", "Bordeaux", "Juventus", "Real Madrid"], "France", "MID", 0 , "right")
 const beckham = new Footballer("David Beckham", 50, ["Premier League","La Liga","Serie A","Ligue 1","MLS"], ["Manchester United", "Real Madrid","AC Milan","Paris Saint-Germain", "LA Galaxy"], "England", "MID", 0 , "right")
 
+const db = new sqlite.Database('footballersDB.db', (err) => { if (err) throw err })
 
 function Container(){
-    this.footballers = []
 
-    this.add = function(footballer){
-        this.footballers.push(footballer)
+    ///////////////////////             LAB 01           ////////////////////////
+    // this.footballers = []
+
+    // this.add = function(footballer){
+    //     this.footballers.push(footballer)
+    // }
+
+    // this.findName = function(name){
+    //     return this.footballers.find(footballer => footballer.name === name)
+    // }
+
+    // this.findAge = function(age){
+    //     return this.footballers.filter(footballer => footballer.age === age)
+    // }
+
+    // this.findLeagues = function(league){ 
+    //     return this.footballers.filter(footballer => footballer.leagues.includes(league))
+    // }
+
+    // this.findTeam = function(team){ 
+    //     return this.footballers.filter(footballer => footballer.teams.includes(team))
+    // }
+
+    // this.findNationality = function(nationality){
+    //     return this.footballers.filter(footballer => footballer.nationality === nationality)
+    // }
+
+    // this.findPosition = function(position){
+    //     return this.footballers.filter(footballer => footballer.position === position)
+    // }
+
+    // this.findFoot = function(foot){
+    //     return this.footballers.filter(footballer => footballer.foot === foot)
+    // }
+
+    // this.findCareer = function(career){
+    //     return this.footballers.filter(footballer => footballer.career === career)
+    // }
+
+    /////////////////////////             LAB 02              //////////////////////
+
+    // NAME
+    this.findName = () => {
+            return new Promise((resolve, reject) => {
+                // query DB and return an array of all answers to this question
+                const sql =
+                    `SELECT *
+                    FROM footballers
+                    WHERE footballers.Name = ?`
+    
+                db.all(sql, [this.name], (err, rows) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        const result = rows.map((item) => new Footballer(item.name, item.age, item.leagues,
+                            item.teams, item.nationality, item.position, item.career, item.foot))
+    
+                        resolve(result)
+                    }
+                })
+    
+            })
+        }
+
+    // AGE
+    this.findAge = () => {
+        return new Promise((resolve, reject) => {
+            // query DB and return an array of all answers to this question
+            const sql =
+                `SELECT *
+                FROM footballers 
+                WHERE footballers.Age = ?`
+
+            db.all(sql, [this.age], (err, rows) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    const result = rows.map((item) => new Footballer(item.name, item.age, item.leagues,
+                        item.teams, item.nationality, item.position, item.career, item.foot))
+
+                    resolve(result)
+                }
+            })
+
+        })
     }
 
-    this.findName = function(name){
-        return this.footballers.find(footballer => footballer.name === name)
+    // LEAGUES
+    this.findLeagues = () => {
+        return new Promise((resolve, reject) => {
+            // query DB and return an array of all footballers that played in this league
+            const sql =
+                `SELECT *
+                FROM footballers 
+                WHERE footballers.Leagues LIKE ?`
+
+            const params = ['%"${league}"%'];
+
+            db.all(sql, params, (err, rows) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    const result = rows.map((item) => new Footballer(item.name, item.age, item.leagues,
+                        item.teams, item.nationality, item.position, item.career, item.foot))
+
+                    resolve(result)
+                }
+            })
+
+        })
     }
 
-    this.findAge = function(age){
-        return this.footballers.filter(footballer => footballer.age === age)
+    // TEAMS
+    this.findTeams = () => {
+        return new Promise((resolve, reject) => {
+            // query DB and return an array of all footballers that played for this team
+            const sql =
+                `SELECT *
+                FROM footballers 
+                WHERE footballers.Teams LIKE ?`
+
+            const params = ['%"${team}"%'];
+            
+            db.all(sql, params, (err, rows) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    const result = rows.map((item) => new Footballer(item.name, item.age, item.leagues,
+                        item.teams, item.nationality, item.position, item.career, item.foot))
+
+                    resolve(result)
+                }
+            })
+
+        })
     }
 
-    this.findLeagues = function(league){ 
-        return this.footballers.filter(footballer => footballer.leagues.includes(league))
+    // NATIONALITY
+    this.findNationality = () => {
+        return new Promise((resolve, reject) => {
+            // query DB and return an array of all answers to this question
+            const sql =
+                `SELECT *
+                FROM footballers
+                WHERE footballers.Nationality = ?`
+
+            db.all(sql, [this.nationality], (err, rows) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    const result = rows.map((item) => new Footballer(item.name, item.age, item.leagues,
+                        item.teams, item.nationality, item.position, item.career, item.foot))
+
+                    resolve(result)
+                }
+            })
+
+        })
     }
 
-    this.findTeam = function(team){ 
-        return this.footballers.filter(footballer => footballer.teams.includes(team))
+    // POSITION
+    this.findPosition = () => {
+        return new Promise((resolve, reject) => {
+            // query DB and return an array of all answers to this question
+            const sql =
+                `SELECT *
+                FROM footballers
+                WHERE footballers.Position = ?`
+
+            db.all(sql, [this.position], (err, rows) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    const result = rows.map((item) => new Footballer(item.name, item.age, item.leagues,
+                        item.teams, item.nationality, item.position, item.career, item.foot))
+
+                    resolve(result)
+                }
+            })
+
+        })
     }
 
-    this.findNationality = function(nationality){
-        return this.footballers.filter(footballer => footballer.nationality === nationality)
+    // CAREER   
+    this.findCareer = () => {
+        return new Promise((resolve, reject) => {
+            // query DB and return an array of all answers to this question
+            const sql =
+                `SELECT *
+                FROM footballers
+                WHERE footballers.Career = ?`
+
+            db.all(sql, [this.career], (err, rows) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    const result = rows.map((item) => new Footballer(item.name, item.age, item.leagues,
+                        item.teams, item.nationality, item.position, item.career, item.foot))
+
+                    resolve(result)
+                }
+            })
+
+        })
     }
 
-    this.findPosition = function(position){
-        return this.footballers.filter(footballer => footballer.position === position)
+    // FOOT
+    this.findFoot = () => {
+        return new Promise((resolve, reject) => {
+            // query DB and return an array of all answers to this question
+            const sql =
+                `SELECT *
+                FROM footballers
+                WHERE footballers.Foot = ?`
+
+            db.all(sql, [this.foot], (err, rows) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    const result = rows.map((item) => new Footballer(item.name, item.age, item.leagues,
+                        item.teams, item.nationality, item.position, item.career, item.foot))
+
+                    resolve(result)
+                }
+            })
+
+        })
     }
 
-    this.findFoot = function(foot){
-        return this.footballers.filter(footballer => footballer.foot === foot)
-    }
-
-    this.findCareer = function(career){
-        return this.footballers.filter(footballer => footballer.career === career)
-    }
-
+    
 }
 
 
-const footballers = new Container()
+// testing
 
-footballers.add(ronaldo)
-footballers.add(messi)
-footballers.add(maldini)
-footballers.add(zidane)
-footballers.add(beckham)
+const cl = new Container()
+cl.findNationality("Argentina")
+    .then((list) => { console.log('We have ', list.length, ' footballers') })
+    .catch((err) => { console.log("ERROR: ", err) })
 
-console.log(footballers.findLeagues("La Liga"))
+
